96a50c04ddb66d62dab4d18792c2837a58ab9d80
Ademir Ogliari <ad.ogliari@hotmail.com>
2023-09-18 16:31:36 -0300

    Finalizada sessão 03

---
+++ s03-Introdução/s03a12_recebendo-dados-usuario.py
@@ -0,0 +1,15 @@
+"""
+Recebendo dados do usuário
+
+input() -> Todo dado recebido via input é do tipo string
+
+"""
+
+# Entrada de dados
+nome = input('Qual seu nome? \n')
+idade = int(input('Qual sua idade? \n'))
+
+# Processamento
+
+# Saída de dados
+print(f'Seja bem-vindo(a) {nome}, você tem {idade} anos. ')
